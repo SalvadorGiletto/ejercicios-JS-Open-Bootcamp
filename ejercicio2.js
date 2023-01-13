8a6141e407840459567dc5b086c7416f50e62377
Salvador Giletto <salvagiletto@gmail.com>
2023-01-13 17:41:47 -0300

    ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,13 @@
+const salvador = [
+    nombre = "Salvador",
+    edad = 29,
+    desarrollador = true,
+    nacimiento = new Date (1993, 1, 9),
+    libro = {
+        titulo: "Quien se ha robado mi queso",
+        autor: "Spencer Jhonson",
+        fecha: new Date(2003, 0, 1),
+    }
+]
+
+console.log(salvador)
